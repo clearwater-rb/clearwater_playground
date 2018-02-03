c42c8d223f2ee5f7c702f37aba6a1dcc336e9e64
Jamie Gaskins <jgaskins@gmail.com>
2018-02-03 16:08:47 -0500

    Add Ruby date stdlib to boilerplate

--- assets/js/playground_boilerplate.rb
+++ assets/js/playground_boilerplate.rb
@@ -1,4 +1,8 @@
+# Everything that will need to be loaded goes here. We don't currently have a
+# way to load arbitrary Ruby libraries.
+
 require 'opal'
+require 'date'
 require 'clearwater'
 require 'grand_central'
 require 'clearwater/memoized_component'
